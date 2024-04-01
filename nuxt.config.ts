1e7b019dca2d3b537907f0a36a7d59ff45aa3579
박진성 <mac9692@nate.com>
2024-04-01 15:57:12 +0900

    feat: config
    1. Type Check
    2. Quasar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: [
+    'nuxt-quasar-ui'
+  ],
+  quasar: { /* */},
+  devtools: { enabled: true },
+  typescript: {
+    typeCheck: true
+  }
 })
